11a38fc5428383bcd8e991491365740fe52a8122
TheArenter <37928902+TheArenter@users.noreply.github.com>
2018-06-21 23:51:42 +0200

    Add files via upload

---
+++ execute.php
@@ -0,0 +1,38 @@
+<?php
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+if(!$update)
+{
+  exit;
+}
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+$text = trim($text);
+$text = strtolower($text);
+header("Content-Type: application/json");
+$response = '';
+if(strpos($text, "/start") === 0 || $text=="ciao")
+{
+	$response = "La difficoltà EX dei conigli è disponibile nei seguenti orari: 00:00/05:00/13:00/16:00";
+}
+elseif($text=="/conigli")
+{
+	$response = "La difficoltà EX dei conigli è disponibile nei seguenti orari: 00:00/05:00/13:00/16:00";
+}
+elseif($text=="domanda 2")
+{
+	$response = "risposta 2";
+}
+else
+{
+	$response = "";
+}
+$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
