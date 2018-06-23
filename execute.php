a21e335043c0d203a29fd72e2647f79cc2c20729
TheArenter <37928902+TheArenter@users.noreply.github.com>
2018-06-23 12:56:03 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -28,31 +28,6 @@
 			sendMessage($UserChatId, $msg);
 			break;
 
-		/*case '/Tastiera': // Command to show normal Keyboard
-			$msg = "This is a Tutorial, this Keyboard has 3 buttons, click one to test.";
-			showKeyboard($UserChatId, $msg);
-			break;
-
-		case "chatid":
-			$msg = $UserChatId;
-			sendMessage($UserChatId, $msg);
-			break;
-
-		case "Normal Keyboard": // This is the same text inside a Keyboard
-			$msg = "Abracadabra and keyboard will appear!";
-			showKeyboard($UserChatId, $msg);
-			break;
-
-		case "Inline Keyboard": // This is the same text inside a Keyboard
-			$msg = "Abracadabra and inline keyboard will appear!";
-			inlineKeyboard($UserChatId, $msg);
-			break;
-
-		case "Remove Keyboard": // This is the same text inside a Keyboard
-			$msg = "Abracadabra and keyboard will disappear!";
-			removeKeyboard($UserChatId, $msg);
-			break;*/
-
 		default:
 			$msg = "Unknown Command! So sorry ;(";
 			sendMessage($ChatId, $msg);
@@ -66,25 +41,5 @@
 		file_get_contents($url);
 	}
 
-	/*function showKeyboard($chat_id, $text)
-	{
-		$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Normal%20Keyboard"],["Hide%20Keyboard","Remove%20Keyboard"]],"one_time_keyboard":true}';
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-		file_get_contents($url);
-	}
-
-	function removeKeyboard($chat_id, $text)
-	{
-		$jSonCodeKeyboard = '&reply_markup={"remove_keyboard":true}';
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-		file_get_contents($url);
-	}
-
-	function inlineKeyboard($chat_id, $text) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
-	{
-		$jSonCodeKeyboard = '&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots/api"},{"text":"Google","url":"https://www.google.com"}]]}';
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
-		file_get_contents($url);
-	}*/
 
 ?>
