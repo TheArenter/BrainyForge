ed9895902dd67f4482b36c70a67846958aec5679
TheArenter <37928902+TheArenter@users.noreply.github.com>
2018-06-23 12:41:08 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,39 +1,89 @@
 <?php
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
-if(!$update)
-{
-  exit;
-}
-$message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$needle = 'buongiorno';
-$text = isset($message['text']) ? $message['text'] : "";
-$text = trim($text);
-$text = strtolower($text);
-header("Content-Type: application/json");
-$response = '';
-if(strpos($text, "/start") === 0)
-{
-	$response = "Ciao $firstname";
-}
-elseif(strpos($text, "/conigli") === 0) 
-{
-	$response = "La difficoltà EX dei conigli è disponibile nei seguenti orari: 00:00/05:00/13:00/16:00";
-}
-elseif(strpos($text,$needle) !== false) 
-{
-	$response = "Buongiorno a te $firstname";
-}
-else
-{
-	$response = "";
-}
-$parameters = array('chat_id' => $chatId, "text" => $response);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+	
+	$botToken = "590748562:AAHVXJMYVo67rq7ZfGXAzMyVPfg0dii5cok"; // Api TOKEN to our bot
+	$website = "https://api.telegram.org/bot".$botToken;
+
+	$FilejSon = file_get_contents("php://input"); // Take the url input, in this case will be executed method getUpdates that return Update.
+	$FilejSon = json_decode($FilejSon, TRUE); // Decode the variable before because now we can search with key (because it's a dictionary)
+
+	$FirstName = $FilejSon["message"]["chat"]["first_name"]; // Get the name that user set
+	$UserChatId = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
+	$Message = $FilejSon["message"]["text"]; // Get the message sent from user
+
+	switch ($Message)
+	{
+		case '/start':
+			$msg = "Welcome $GLOBALS[FirstName]!";
+			showKeyboard($UserChatId, $msg);
+			break;
+		
+		case '/Conigli':
+			$msg = "La difficoltà EX dei conigli è disponibile nei seguenti orari: 00:00/05:00/13:00/16:00";
+			showKeyboard($UserChatId, $msg);
+			break;
+			
+		case 'Buongiorno':
+			$msg = "Buongiorno $GLOBALS[FirstName]!";
+			showKeyboard($UserChatId, $msg);
+			break;
+
+		case '/Tastiera': // Command to show normal Keyboard
+			$msg = "This is a Tutorial, this Keyboard has 3 buttons, click one to test.";
+			showKeyboard($UserChatId, $msg);
+			break;
+
+		case "chatid":
+			$msg = $UserChatId;
+			sendMessage($UserChatId, $msg);
+			break;
+
+		case "Normal Keyboard": // This is the same text inside a Keyboard
+			$msg = "Abracadabra and keyboard will appear!";
+			showKeyboard($UserChatId, $msg);
+			break;
+
+		case "Inline Keyboard": // This is the same text inside a Keyboard
+			$msg = "Abracadabra and inline keyboard will appear!";
+			inlineKeyboard($UserChatId, $msg);
+			break;
+
+		case "Remove Keyboard": // This is the same text inside a Keyboard
+			$msg = "Abracadabra and keyboard will disappear!";
+			removeKeyboard($UserChatId, $msg);
+			break;
+
+		default:
+			$msg = "Unknown Command! So sorry ;(";
+			sendMessage($ChatId, $msg);
+			break;
+	} 
+	
+
+	function sendMessage($chat_id, $text)
+	{
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text);
+		file_get_contents($url);
+	}
+
+	function showKeyboard($chat_id, $text)
+	{
+		$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Normal%20Keyboard"],["Hide%20Keyboard","Remove%20Keyboard"]],"one_time_keyboard":true}';
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
+		file_get_contents($url);
+	}
+
+	function removeKeyboard($chat_id, $text)
+	{
+		$jSonCodeKeyboard = '&reply_markup={"remove_keyboard":true}';
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
+		file_get_contents($url);
+	}
+
+	function inlineKeyboard($chat_id, $text) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
+	{
+		$jSonCodeKeyboard = '&reply_markup={"inline_keyboard":[[{"text":"API%20Bot%20Telegram","url":"https://core.telegram.org/bots/api"},{"text":"Google","url":"https://www.google.com"}]]}';
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text).$jSonCodeKeyboard;
+		file_get_contents($url);
+	}
+
+?>
